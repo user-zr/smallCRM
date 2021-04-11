b5a07935b56bdba7af2d5e594833c83d34bfa262
23634 <23634@DESKTOP-4FRF9J0>
2021-04-11 13:54:21 +0800

    1

---
+++ src/test/java/com/zr/smallcrm/SmallcrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zr.smallcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallcrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
